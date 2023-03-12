84f6ae64cefcefca700322d19f374a5bb07f7754
jessica <jvictoriaw@hotmail.co.uk>
2023-03-12 14:12:20 +0000

    task 1 - linking JS and creating variables

---
+++ session1/main.js
@@ -0,0 +1,6 @@
+const myName = 'Jessica';
+let myAge = 26;
+const futureJob = 'web developer'
+let aboutMe = `My name is ${myName}, I am ${myAge} years old and I will soon become a ${futureJob}!`
+
+console.log(aboutMe)

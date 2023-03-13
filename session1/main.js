d32ee24324ab5f1fd7b85302cbefd5c36f2469e0
jessica <jvictoriaw@hotmail.co.uk>
2023-03-13 15:22:37 +0000

    exercise 2.0

--- session1/main.js
+++ session1/main.js
@@ -4,3 +4,7 @@ const futureJob = 'web developer'
 let aboutMe = `My name is ${myName}, I am ${myAge} years old and I will soon become a ${futureJob}!`
 
 console.log(aboutMe)
+
+let favouriteColours = ['red', 'pink', 'black', 'green', 'blue']
+console.log(favouriteColours[0])
+console.log(favouriteColours[4])

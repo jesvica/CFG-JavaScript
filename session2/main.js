1d06cba3be925f1707d36412683bb24b1d62ea2e
jessica <jvictoriaw@hotmail.co.uk>
2023-03-14 19:45:33 +0000

    conditional statements

---
+++ session2/main.js
@@ -0,0 +1,15 @@
+let genre = 'horror';
+let rating = 5;
+let pillows = 1;
+
+if (rating > 1 && pillows === 1 && genre === 'horror') {
+    console.log('Your friend will watch it!')
+} else if (rating > 1 && genre === 'horror' && pillows === 0) {
+    console.log('Your friend will not watch it!')
+} else if (rating <= 1 && genre === 'horror' && pillows === 1) {
+    console.log('No point watching the movie!')
+} else if (genre !== 'horror') {
+    console.log('Your friend will watch anything!')
+} else {
+    console.log('We can hang out and talk!')
+}

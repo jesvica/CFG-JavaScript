718f09e831c6eb072d0e137ba3f56c91fc6689b7
jessica <jvictoriaw@hotmail.co.uk>
2023-03-23 18:54:46 +0000

    exercise 1.1 task - understanding the dom

---
+++ session3/main.js
@@ -0,0 +1,9 @@
+const paragraph = document.createElement('p')
+paragraph.innerText = 'My name is Jessica';
+paragraph.style.fontSize = '18px';
+paragraph.style.fontFamily = 'sans-serif';
+paragraph.style.width = '100px';
+paragraph.style.border = '1px solid orange';
+paragraph.style.padding = '30px';
+
+document.body.appendChild(paragraph);

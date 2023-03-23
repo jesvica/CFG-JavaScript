63c3342058c66149bdccbffb070d91b071ae5342
jessica <jvictoriaw@hotmail.co.uk>
2023-03-23 19:16:53 +0000

    exercise 1.3 - appendChild and styling

--- session3/main.js
+++ session3/main.js
@@ -7,3 +7,11 @@ paragraph.style.border = '1px solid orange';
 paragraph.style.padding = '30px';
 
 document.body.appendChild(paragraph);
+
+const div = document.getElementById('content')
+const newHeader = document.createElement('h1')
+div.appendChild(newHeader)
+
+newHeader.innerText = `I'm in!`;
+newHeader.style.border = '1px dashed blue';
+newHeader.style.padding = '50px';

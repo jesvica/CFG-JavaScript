f60b95934ee50937792d7b750fc98f96bbf1f302
jessica <jvictoriaw@hotmail.co.uk>
2023-04-01 10:17:11 +0100

    session 3 homework

---
+++ session3/homework/main.js
@@ -0,0 +1,8 @@
+const header = document.getElementById('greeting')
+header.style.textAlign = 'center'
+header.style.fontFamily = 'sans-serif'
+
+
+const image = document.getElementById('mabel')
+image.style.height = '200px'
+image.style.width = 'auto'
